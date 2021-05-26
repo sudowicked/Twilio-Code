da8b3b7dec660575114b6a7429ab5eee28a0f39b
Giannis Nikoloulis <p18niko@ionio.gr>
2021-05-26 22:29:36 +0300

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -1,13 +1,9 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
 import os
 from twilio.rest import Client	
-
-# Find your Account SID and Auth Token at twilio.com/console
 
 account_sid = 'AC02b350c07e80ff5a2e55730d0c5e11ec'
 auth_token = 'f7cab49f8f5f4fee0d838f1833139231'
 client = Client(account_sid, auth_token)
-
 
 message = client.messages.create (
                 body='Hi from twilio-python!',
